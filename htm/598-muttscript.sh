a2bb120e93ef76354cd7c735c62d966739cb066c
Stefan Lukits <sediomyle@gmail.com>
2023-05-01 09:08:28 -0700

    routine update

---
+++ htm/598-muttscript.sh
@@ -0,0 +1,4 @@
+cat 598-saunders.htm | mutt -e 'set content_type=text/html' -s 'rental bill 2023-01' greengrovesbc@gmail.com -a ./598-saunders.htm
+cat 598-mrosen.htm | mutt -e 'set content_type=text/html' -s 'rental bill 2023-01' rosen.michael@gmail.com -a ./598-mrosen.htm
+cat 598-dacruz.htm | mutt -e 'set content_type=text/html' -s 'rental bill 2023-01' desareedacruz@gmail.com -a ./598-dacruz.htm
+cat 598-schen.htm | mutt -e 'set content_type=text/html' -s 'rental bill 2023-01' shirley.x.chen@gmail.com -a ./598-schen.htm
